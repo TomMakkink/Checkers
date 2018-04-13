b00f7be6a3cdc4dd69cbe1ef316c9d35233ed274
TomMakkink <tommakkink@hotmail.com>
2018-04-13 14:45:43 +0200

    Add test: A board must have 12 black and 12 white pieces

--- Checkers/King.cs
+++ Checkers/King.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace Checkers
 {
-    class King
+    class King : IPiece
     {
+        public Status Status => throw new NotImplementedException();
+
+        public Color Color => throw new NotImplementedException();
+
+        public int Position => throw new NotImplementedException();
+
+        public IEnumerable<int> CapturingMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(int destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> NormalMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
